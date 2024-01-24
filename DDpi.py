c9a6e0b9a5e67b7dccbf2e7ff5b2c52738291654
William Jay <willjay@mit.edu>
2024-01-24 16:39:40 -0500

    Add example with DDpi

---
+++ DDpi.py
@@ -0,0 +1,236 @@
+import numpy as np
+import itertools
+from mhi import mhi
+
+def main():
+
+    bases = {}
+
+    bases[2] = [
+        ("D+", "D+", "pi+"),
+    ]
+
+    bases[1] = [
+        ("D+", "D+", "pi0"),
+        ("D+", "D0", "pi+"),
+        ("D0", "D+", "pi+")
+    ]
+
+    bases[0] = [
+        ("D+", "D+", "pi+"),
+        ("D0", "D0", "pi+"),
+        ("D+", "D0", "pi0"),
+        ("D0", "D+", "pi0"),
+    ]
+
+    # Permutations in S3
+    perms = [np.array(list(perm) + [2]) for perm in itertools.permutations([0,1])]
+
+
+    # Representation matrices for the basis states
+    Dmats = {}
+    for Iz, basis in bases.items():
+        Dmat = np.zeros((len(perms), len(basis), len(basis)))
+        for idx, perm in enumerate(perms):
+            for i, j in itertools.product(range(len(basis)), repeat=2):
+                if basis[i] == tuple(np.array(basis[j])[perm]):
+                    Dmat[idx,i,j] = 1
+        Dmats[Iz] = Dmat
+
+    irreps = {
+        'trivial': np.array([1 for _ in range(len(perms))]),
+        'sign': np.array([mhi.parity(perm) for perm in perms]),
+    }
+
+    # Projections in the group algebra
+    internal_symmetries = {
+        'trivial': [mhi.WeightedPermutation(1, perm) for perm in perms],
+        'sign': [mhi.WeightedPermutation(mhi.parity(perm), perm) for perm in perms],
+    }
+
+    group = mhi.make_oh()
+
+    all_momenta = [
+        np.array([[0,0,0],[0,0,0],[0,0,0]]),
+        np.array([[0,0,1],[0,0,-1],[0,0,0]]),
+        np.array([[0,1,1],[0,-1,-1],[0,0,0]]),
+        np.array([[1,1,1],[-1,-1,-1],[0,0,0]]),
+        np.array([[2,1,0],[-2,-1,0],[0,0,0]]),
+        np.array([[2,1,1],[-2,-1,-1],[0,0,0]]),
+        np.array([[3,2,1],[-3,-2,-1],[0,0,0]]),
+    ]
+    rows = []
+    for momenta in all_momenta:
+        # print("#"*40)
+        little, stab = mhi.make_little_and_stabilizer(momenta, group=group)
+        little_name = mhi.identify_stabilizer(little)
+        stab_name = mhi.identify_stabilizer(stab)
+        little_name = format_group_name(little_name)
+        stab_name = format_group_name(stab_name)
+
+        state = f"$\left|D{tuple(momenta[0])},D{tuple(momenta[1])},pi{tuple(momenta[2])}\\right\\rangle$"
+        state  = state.replace("pi", "\pi")
+        for Iz in [2, 1, 0]:
+            for irrep, irrep_mats in irreps.items():
+                if irrep == 'standard':
+                    subscripts = "rij,rab->ijab"
+                else:
+                    subscripts = "r,rab->ab"
+                fproj = np.einsum(subscripts, irrep_mats, Dmats[Iz])
+                rank = np.linalg.matrix_rank(fproj)
+                if rank > 0:
+                    result, orbit = mhi.mhi(
+                        momenta=momenta,
+                        spin_irreps=['A1m','A1m','A1m'],
+                        internal_symmetry=internal_symmetries[irrep]
+                    )
+                else:
+                    result = {}
+
+                if len(result):
+                    irrep_decomposition = sort_and_format(result.keys())
+                else:
+                    continue #irrep_decomposition = "None"
+
+                columns = [
+                    little_name,
+                    stab_name,
+                    irrep.capitalize(),
+                    state,
+                    str(len(orbit)),
+                    irrep_decomposition
+                ]
+                row = " & ".join(columns) + r"\\"
+                if row not in rows:
+                    rows.append(row)
+    for row in rows:
+        print(row)
+
+
+
+def irrep_priority(irrep_name):
+    """
+    Evaluate the priority of an irrep for sorting.
+
+    Parameters
+    ----------
+    irrep_name : str
+        The irrep name
+
+    Returns
+    -------
+    priority : int
+        The priority of the irrep compared to others, for use in sorting
+    """
+    ordered_irreps = [
+        'A1p', 'A2p', 'Ep', 'T1p', 'T2p',
+        'A1m', 'A2m', 'Em', 'T1m', 'T2m',
+        'A', 'A1', 'A2', 'B', 'B1', 'B2', 'E',
+        "F", "F1", "F2", "G", "G1", "G2", "G1p", "G2p",
+        "Hp", "G1m", "G2m", "Hm"
+    ]
+    priority = dict(zip(ordered_irreps, range(len(ordered_irreps))))
+    if irrep_name not in ordered_irreps:
+        raise ValueError("Bad irrep_name", irrep_name)
+    return priority.get(irrep_name)
+
+def irrep_to_latex(output):
+    """
+    Converts irrep names from internal string representations to latex.
+
+    Parameters
+    ----------
+    output : str
+        Some text containing the internal string representations of the irrep names
+
+    Returns
+    -------
+    output : str
+        The text, but with appropriate replacements, e.g., 'A1p' -> 'A_1^+.
+    """
+    irrep_map = {
+        'A1p': 'A_1^+',
+        'A2p': 'A_2^+',
+        'Ep': 'E^+',
+        'T1p': 'T_1^+',
+        'T2p': 'T_2^+',
+        'A1m': 'A_1^-',
+        'A2m':  'A_2^-',
+        'Em':  'E^-',
+        'T1m':  'T_1^-',
+        'T2m': 'T_2^-',
+        'A': 'A',
+        'A1': 'A_1',
+        'A2': 'A_2',
+        'B':  'B',
+        'B1':  'B_1',
+        'B2':  'B_2',
+        'E': 'E',
+        "F":  "F",
+        "F1":  "F_1",
+        "F2":  "F_2",
+        "G": "G",
+        "G1": "G_1",
+        "G2": "G_2",
+        "G1p": "G_1^+",
+        "G2p": "G_2^+",
+        "Hp": "H^+",
+        "G1m": "G_1^-",
+        "G2m": "G_2^-",
+        "Hm": "H^-",
+    }
+    for old, new in irrep_map.items():
+        output = output.replace(old, new)
+    return output
+
+
+def format_group_name(name):
+    name_map = {
+        'C2R': 'C_2^R',
+        'C2P': 'C_2^P',
+        'Oh': 'O_h',
+        'C4v': 'C_4^v',
+        'C3v': 'C_3^v',
+        'C2v': 'C_2^v',
+        'C1': 'C_1'
+    }
+    return '$' + name_map[name] + '$'
+
+def sort_and_format(irrep_list):
+    """
+    TODO
+    """
+    # Count the degenerate copies
+    irrep_counts = {}
+    for irrep, _ in irrep_list:
+        if irrep not in irrep_counts:
+            irrep_counts[irrep] = 1
+        else:
+            irrep_counts[irrep] += 1
+
+    # Sort the names according to a conventional ordering
+    names = list(irrep_counts.keys())
+    degeneracies = list(irrep_counts.values())
+    names, degeneracies = zip(
+            *sorted(zip(names, degeneracies),
+            key=lambda pair: irrep_priority(pair[0])))
+
+    # Convert the results to a latex string
+    output = []
+    for k, name in zip(degeneracies, names):
+        if k == 1:
+            output.append(name)  # "1" looks silly as a coefficient
+        else:
+            output.append(str(k)+name)
+    output = r' \oplus '.join(output)
+    output = irrep_to_latex(output)
+    output = '$' + output + '$'
+    return output
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
